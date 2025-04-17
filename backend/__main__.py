80066e16344e89d56ae6489953f24b3b57042192
xgro <dlwocks13@gmail.com>
2025-04-17 20:09:34 +0900

    feat: FastAPI를 사용한 헬스 체크 엔드포인트 추가 및 uvicorn으로 서버 실행 설정

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,9 +1,26 @@
 from backend.settings import settings
 
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
 
 def main():
-    print("Hello from backend!")
-    print(f"IAM Access Key Expiration: {settings.iam_access_key_expiration}")
+    uvicorn.run(
+        "backend.__main__:app",
+        workers=settings.workers_count,
+        host=settings.host,
+        port=settings.port,
+        reload=settings.reload,
+        log_level=settings.log_level.value.lower(),
+        # factory=True,
+    )
 
 
 if __name__ == "__main__":
